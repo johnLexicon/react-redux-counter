4942cd395c3b2421cf51aef4c4acceec6d0c7e0d
John Lundgren <45228836+johnLexicon@users.noreply.github.com>
2021-12-06 13:05:05 +0100

    store and reducer implementation.

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'redux';
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'INCREMENT') {
+    return state.counter + 1;
+  }
+  if (action.type === 'DECREMENT') {
+    return state.counter - 1;
+  }
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
