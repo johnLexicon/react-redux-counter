20778ee9f6e7f590e201e3c0dedd2968fb8544d9
John Lundgren <45228836+johnLexicon@users.noreply.github.com>
2021-12-06 14:36:19 +0100

    Added incr and decr functionality with dispatch hook.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,31 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux'; // The useSelector hook creates automatically a subscription
 
+// When the component is unmounted redux will automatically clear the subscription.
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+  const handleDecrement = () => {
+    dispatch({ type: 'DECREMENT' });
+  };
   return (
     <div className="counter-wrapper text-center py-5">
+      <div className="h3 text-muted">Redux Counter</div>
       <div className="h2">{counter}</div>
-      <button className="btn btn-primary mt-3">Toggle Counter</button>
+      <div className="counter-btns">
+        <button onClick={handleIncrement} className="btn btn-primary m-2">
+          Increment
+        </button>
+        <button onClick={handleDecrement} className="btn btn-primary m-2">
+          Decrement
+        </button>
+      </div>
+      <div className="toggle-wrapper mt-3">
+        <button className="btn btn-primary">Toggle Counter</button>
+      </div>
     </div>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,10 @@ import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === 'INCREMENT') {
-    return state.counter + 1;
+    return { ...state, counter: state.counter + 1 };
   }
   if (action.type === 'DECREMENT') {
-    return state.counter - 1;
+    return { ...state, counter: state.counter - 1 };
   }
   return state;
 };
